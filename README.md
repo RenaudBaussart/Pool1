4328f3bcf57adbf76effe8db7dcace45704cd662
Renaud Baussart <renaud.baussart@proton.me>
2025-11-17 15:58:07 +0100

    Ex_05

---
+++ README.md
@@ -0,0 +1,2 @@
+origin	git@github.com:RenaudBaussart/Pool1.git (fetch)
+origin	git@github.com:RenaudBaussart/Pool1.git (push)
--- ex_05.sh
+++ ex_05.sh
@@ -1,6 +1,6 @@
 git config --global user.name "Renaud Baussart"
 git config --global user.email "renaud.baussart@proton.me"
-git remote -V > README.md
+git remote -v > README.md
 git add -A
 git commit -m "Ex_05"
 git push
