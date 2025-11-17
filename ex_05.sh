99bcc0c3d76ba5520585b80af606192051d9347e
Renaud Baussart <renaud.baussart@proton.me>
2025-11-17 15:56:36 +0100

    Ex_05

--- ex_05.sh
+++ ex_05.sh
@@ -1,6 +1,6 @@
 git config --global user.name "Renaud Baussart"
 git config --global user.email "renaud.baussart@proton.me"
-mmd -t sample.txt -o README.md
+echo"hello" > README.md
 git add -A
 git commit -m "Ex_05"
 git push
